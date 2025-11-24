19368cc3ae27c0f0387df06237859f03ebabb8b3
Erackson souza <souza.erackson@gmail.com>
2025-11-24 18:35:36 -0300

    feat: add database connection

---
+++ src/database.ts
@@ -0,0 +1,9 @@
+import knex from "knex";
+
+export const database = knex({
+  client: "sqlite3",
+  connection: {
+    filename: "./tmp/app.db",
+  },
+  useNullAsDefault: true,
+});
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import fastify from "fastify";
+import { database } from "./database";
 
 const app = fastify();
 
-app.get("/hello", () => {
-  return { message: "Hello, World!" };
+app.get("/hello", async () => {
+  const tables = await database("sqlite_schema").select("*");
+
+  return tables;
 });
 
-app.listen({
+app
+  .listen({
     port: 4444,
-}).then(() => {
-    console.log('Servidor rodando na porta 4444 ');
-    
-})
+  })
+  .then(() => {
+    console.log("Servidor rodando na porta 4444 ");
+  });
